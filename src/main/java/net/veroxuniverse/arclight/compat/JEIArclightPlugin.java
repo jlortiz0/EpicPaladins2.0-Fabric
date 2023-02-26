af02b4e6a7bfb281881958a2189a2036ccc3657b
Tarantel <SerienKid@live.de>
2023-02-26 21:09:20 +0100

    Update JEIArclightPlugin.java

--- src/main/java/net/veroxuniverse/arclight/compat/JEIArclightPlugin.java
+++ src/main/java/net/veroxuniverse/arclight/compat/JEIArclightPlugin.java
@@ -3,13 +3,18 @@ package net.veroxuniverse.arclight.compat;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeManager;
 import net.veroxuniverse.arclight.ArclightMod;
+import net.veroxuniverse.arclight.init.BlocksInit;
 import net.veroxuniverse.arclight.recipe.ArmorForgeRecipe;
+import net.veroxuniverse.arclight.screen.ArmorForgeScreen;
 
 import java.util.List;
 import java.util.Objects;
@@ -36,5 +41,17 @@ public class JEIArclightPlugin implements IModPlugin {
 
         List<ArmorForgeRecipe> recipesInfusing = rm.getAllRecipesFor(ArmorForgeRecipe.Type.INSTANCE);
         registration.addRecipes(INFUSION_TYPE, recipesInfusing);
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration){
+        var soulbreeder = new ItemStack(BlocksInit.ARMOR_FORGE.get());
+        registration.addRecipeCatalyst(soulbreeder, ArmorForgeRecipeCategory.RECIPE_TYPE);
+    }
+
+    @Override
+    public void registerGuiHandlers(IGuiHandlerRegistration registration)
+    {
+        registration.addRecipeClickArea(ArmorForgeScreen.class, 105, 33, 10, 40, JEIArclightPlugin.INFUSION_TYPE);
     }
 }
